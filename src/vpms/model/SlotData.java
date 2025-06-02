d51eb7afa8bcdeb287eccac76bc2f01dce378ba5
Rupeshycho <crrupesh0@gmail.com>
2025-06-02 12:58:57 +0545

    Added VehicleTypeAndPriceData table and insertion

---
+++ src/vpms/model/SlotData.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package vpms.model;
+
+/**
+ *
+ * @author Rupes
+ */
+public class SlotData {
+    private int id;
+    private String slotName;
+    private String vehicleType;
+    private int numberOfSlot;
+    private String status;
+}
