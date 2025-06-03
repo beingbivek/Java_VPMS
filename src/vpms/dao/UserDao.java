555e08c81fc1e503edd1cd81c4992291f1f654bc
Prabhash39 <prabhashsigdel10@gmail.com>
2025-06-03 14:19:41 +0545

    Revert "Merge remote-tracking branch 'origin/dev' into prabhash"
    This reverts commit e99cbabf3518930d08fa0b220d6586d6775fedd7, reversing
    changes made to f11b04107e4b663a5a51c28a0fd3dd8716fa5e56.

--- src/vpms/dao/UserDao.java
+++ src/vpms/dao/UserDao.java
@@ -26,10 +26,9 @@ public class UserDao {
             + "type VARCHAR(20) NOT NULL, "
             + "email VARCHAR(100) UNIQUE NOT NULL, "
             + "password VARCHAR(255) NOT NULL, "
-            + "phone VARCHAR(10) NOT NULL "  
             + "image BLOB"
             + ")";
-         String query=  "INSERT INTO vpmsUsers (name, type, email, password,phone,image) VALUES (?,?, ?, ?,?,?)";
+         String query=  "INSERT INTO vpmsUsers (name, type, email, password,image) VALUES (?,?, ?, ?,?)";
          
         try {
             PreparedStatement createtbl= conn.prepareStatement(createTableSQL);
@@ -43,8 +42,7 @@ public class UserDao {
             pstmt.setString(2, userData.getEmail());
             pstmt.setString(3, userData.getType());
             pstmt.setString(4, userData.getPassword());
-            pstmt.setString(5, userData.getPhone());
-            pstmt.setBytes(6, userData.getImage());
+            pstmt.setBytes(5, userData.getImage());
             int result = pstmt.executeUpdate();
             return result > 0;
         } catch (SQLException ex) {
@@ -69,7 +67,6 @@ public class UserDao {
                     result.getString("type"),
                     result.getString("email"),
                     result.getString("password"),
-                    result.getString("phone"),
                     result.getBytes("image")                 
                 );
                 user.setId(result.getInt("id"));
@@ -97,7 +94,6 @@ public class UserDao {
                 result.getString("type"),
                 result.getString("email"),
                 result.getString("password"),
-                result.getString("phone"),
                 result.getBytes("image")
             );
             user.setId(result.getInt("id"));
