427a6e6c2404957c2f145503432346e8219cd2b2
Prabhash39 <prabhashsigdel10@gmail.com>
2025-06-04 21:00:27 +0545

    Fixed merge errors

--- src/vpms/view/AddVehiclesView.java
+++ src/vpms/view/AddVehiclesView.java
@@ -10,18 +10,18 @@ import java.util.List;
 import java.util.Arrays;
 
 
-import vpms.controller.AddParkingVehiclesController;
+import vpms.controller.AddVehiclesController;
 
 /**
  *
  * @author PRABHASH
  */
-public class AddParkingVehiclesView extends javax.swing.JFrame {
+public class AddVehiclesView extends javax.swing.JFrame {
 
     /**
      * Creates new form AddParkingVehiclesView
      */
-    public AddParkingVehiclesView() {
+    public AddVehiclesView() {
         initComponents();
     }
 
@@ -183,7 +183,7 @@ public class AddParkingVehiclesView extends javax.swing.JFrame {
     }// </editor-fold>//GEN-END:initComponents
 
     private void btnSaveVehicleActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnSaveVehicleActionPerformed
-     AddParkingVehiclesController controller = new AddParkingVehiclesController(this);
+     AddVehiclesController controller = new AddVehiclesController(this);
     controller.handleSaveVehicle(); 
     
         
@@ -219,20 +219,21 @@ public class AddParkingVehiclesView extends javax.swing.JFrame {
                 }
             }
         } catch (ClassNotFoundException ex) {
-            java.util.logging.Logger.getLogger(AddParkingVehiclesView.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(AddVehiclesView.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (InstantiationException ex) {
-            java.util.logging.Logger.getLogger(AddParkingVehiclesView.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(AddVehiclesView.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (IllegalAccessException ex) {
-            java.util.logging.Logger.getLogger(AddParkingVehiclesView.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(AddVehiclesView.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (javax.swing.UnsupportedLookAndFeelException ex) {
-            java.util.logging.Logger.getLogger(AddParkingVehiclesView.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(AddVehiclesView.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         }
+        //</editor-fold>
         //</editor-fold>
 
         /* Create and display the form */
         java.awt.EventQueue.invokeLater(new Runnable() {
             public void run() {
-                new AddParkingVehiclesView().setVisible(true);
+                new AddVehiclesView().setVisible(true);
             }
         });
     }
