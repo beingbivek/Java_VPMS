12b2effc5fe599a01c3599adb2c8f29fe5d3b480
Chandani <karojna30@gmail.com>
2025-06-09 14:09:57 +0545

    added ParkingFeeController

--- src/vpms/controller/ParkingFeeController.java
+++ src/vpms/controller/ParkingFeeController.java
@@ -3,6 +3,9 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
 package vpms.controller;
+import vpms.dao.ParkingDao;
+import vpms.view.ParkingPaymentView;
+
 
 
 /**
@@ -10,5 +13,33 @@ package vpms.controller;
  * @author Chandani
  */
 public class ParkingFeeController {
+     private ParkingPaymentView view;
+    private ParkingDao parkingDao;
+public ParkingFeeController (ParkingPaymentView view) {
+    this.view = view;
+    this.parkingDao = new ParkingDao();
+    
+}
+public void open(){
+    this.view.setVisible(true);
    
 }
+public void close(){
+    this.view.dispose();
+}
+//public BigDecimal generateBill(int ticketId) throws SQLException {
+//        Parking ticket = dao.getTicketById(ticketId);
+//        if (ticket == null || ticket.getExitTime() == null) {
+//            throw new RuntimeException("Invalid ticket or vehicle not exited yet.");
+//        }
+//        ParkingRate rate = dao.getCurrentRate();
+//        return FeeCalculator.calculateFee(ticket, rate);
+//    }
+
+
+}
+
+
+
+   
+
