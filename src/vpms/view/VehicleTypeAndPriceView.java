bfe7397d44dee9ecd1dc9cd65bf576ef44003d77
Prabhash39 <prabhashsigdel10@gmail.com>
2025-06-09 21:22:21 +0545

    connected vehicle Type View with controller properly

--- src/vpms/view/VehicleTypeAndPriceView.java
+++ src/vpms/view/VehicleTypeAndPriceView.java
@@ -185,6 +185,7 @@ public class VehicleTypeAndPriceView extends javax.swing.JFrame {
      * @param args the command line arguments
      */
     public static void main(String args[]) {
+        
         /* Set the Nimbus look and feel */
         //<editor-fold defaultstate="collapsed" desc=" Look and feel setting code (optional) ">
         /* If Nimbus (introduced in Java SE 6) is not available, stay with the default look and feel.
@@ -203,7 +204,11 @@ public class VehicleTypeAndPriceView extends javax.swing.JFrame {
         //</editor-fold>
 
         /* Create and display the form */
-        java.awt.EventQueue.invokeLater(() -> new VehicleTypeAndPriceView().setVisible(true));
+        java.awt.EventQueue.invokeLater(() -> {
+    VehicleTypeAndPriceView view = new VehicleTypeAndPriceView();
+    new vpms.controller.VehicleTypeAndPriceController(view); 
+    view.setVisible(true);
+});
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
@@ -234,4 +239,6 @@ public JButton getSaveButton() {
 public JButton getDeleteButton() {
     return deleteButton;
 }
+
+
 }
