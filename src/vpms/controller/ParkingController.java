81962516cb912b773ac593c5e28136d6443b36b0
Chandani <karojna30@gmail.com>
2025-06-11 00:34:40 +0545

    update parkingController

--- src/vpms/controller/ParkingController.java
+++ src/vpms/controller/ParkingController.java
@@ -4,10 +4,27 @@
  */
 package vpms.controller;
 
+import vpms.dao.ParkingDao;
+import vpms.view.EntryView;
+
 /**
  *
  * @author Chandani
  */
 public class ParkingController {
+   private final EntryView view;
+    private final ParkingDao parkingDao;
+
+    public ParkingController(EntryView view) { //constructor
+        this.view = view;
+        this.parkingDao = new ParkingDao();
+}
     
+    public void open() {
+        view.setVisible(true);
+    }
+
+    public void close() {
+        view.dispose();
+    }
 }
