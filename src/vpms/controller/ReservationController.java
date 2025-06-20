f5eb359cb62bc65372cf2afd73069d3ccd68c09b
Prabhash39 <prabhashsigdel10@gmail.com>
2025-06-20 19:31:02 +0545

    ReservationController added

--- src/vpms/controller/ReservationController.java
+++ src/vpms/controller/ReservationController.java
@@ -3,63 +3,18 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
 package vpms.controller;
-import java.awt.event.*;
-import java.util.List;
-import javax.swing.table.DefaultTableModel;
 
 import vpms.dao.ReservationDao;
 import vpms.model.ReservationData;
+import vpms.view.AddReservationView;
+import vpms.view.EditReservationView;
 import vpms.view.ReservationView;
+
+import javax.swing.*;
+import javax.swing.table.DefaultTableModel;
+import java.awt.event.*;
+import java.util.List;
 /**
  *
  * @author PRABHASH
  */
-public class ReservationController {
-    private ReservationView view;
-    private ReservationDao dao;
-
-    public ReservationController(ReservationView view) {
-        this.view = view;
-        this.dao = new ReservationDao();
-
-        loadTable();
-        addListeners();
-    }
-
-    public void open() {
-        view.setVisible(true);
-    }
-
-    private void loadTable() {
-        List<ReservationData> list = dao.getAllReservations();
-        DefaultTableModel model = (DefaultTableModel) view.getReservationTable().getModel();
-        model.setRowCount(0);
-
-        if (list != null) {
-            for (ReservationData data : list) {
-                model.addRow(new Object[]{
-                        data.getId(),
-                        data.getUserId(),
-                        data.getVehicleId(),
-                        data.getSlotId(),
-                        data.getReservationTime(),
-                        data.getStatus(),
-                        data.getDuration(),
-                        data.getPaymentStatus()
-                });
-            }
-        }
-    }
-
-    private void addListeners() {
-        // Cancel Button Logic
-        view.getCancelButton().addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                view.getSearchField().setText("");
-                loadTable();
-            }
-        });
-}
-    
-}
