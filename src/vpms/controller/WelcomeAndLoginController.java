bc2686474b2d5b3329c56f8761db5d10e012620a
beingbivek <beingbivek@gmail.com>
2025-06-24 19:00:19 +0545

    Now along with email, remember me checkbox status will also be saved during next opening of welcome screen

--- src/vpms/controller/WelcomeAndLoginController.java
+++ src/vpms/controller/WelcomeAndLoginController.java
@@ -4,7 +4,6 @@
  */
 package vpms.controller;
 
-//import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.ItemEvent;
@@ -43,53 +42,56 @@ public class WelcomeAndLoginController {
         this.view.loginUser(new LoginUser());
         this.view.forgotPasswordListener(new ResetPassword());
     }
-    public void open(){
+    public void open() {
         this.view.setVisible(true);
-//        this.view.setUndecorated(true); 
         this.view.setExtendedState(JFrame.MAXIMIZED_BOTH);
         this.view.setResizable(false);
-        this.view.setEmailTextField(getSavedEmail());
-        this.view.viewPassword(new TogglePassword());
-        
-//        Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
-//        int width = screenSize.width;
-//        int height = screenSize.height;
-//        System.out.println(width+"w and h "+height);
-//        this.view.setSize(width, height);
 
+        String[] saved = getSavedEmail();
+        this.view.setEmailTextField(saved[0]);
+        this.view.getRememberCheckBox().setSelected(Boolean.parseBoolean(saved[1]));
+        this.view.viewPassword(new TogglePassword());
     }
+
+    
     public void close(){
         this.view.dispose();
     }
     
-    public void rememberEmail(String email){
-        try{
+    public void rememberEmail(String email) {
+        try {
             Path path = Paths.get(Constants.defaultFileAddress());
-            if (this.view.getRememberCheckBox().isSelected()){
-                if(Files.notExists(path)){
+            String remember = this.view.getRememberCheckBox().isSelected() ? "true" : "false";
+            String content = email + "," + remember;
+            if (this.view.getRememberCheckBox().isSelected()) {
+                if (Files.notExists(path)) {
                     Files.createFile(path);
                 }
-                Files.write(Paths.get(Constants.defaultFileAddress()), email.getBytes(StandardCharsets.UTF_8));
-            }else{
+                Files.write(path, content.getBytes(StandardCharsets.UTF_8));
+            } else {
                 Files.deleteIfExists(path);
             }
-        }catch(Exception ex){
+        } catch (Exception ex) {
             ex.printStackTrace();
         }
     }
+
     
-    public String getSavedEmail(){
-        try{
+    public String[] getSavedEmail() {
+        try {
             Path path = Paths.get(Constants.defaultFileAddress());
-            if(Files.exists(path)){
-                return Files.readString(Paths.get(Constants.defaultFileAddress()));
+            if (Files.exists(path)) {
+                String[] arr = Files.readString(path).split(",", 2);
+                // Ensure array has both values
+                if (arr.length == 2) return arr;
+                if (arr.length == 1) return new String[]{arr[0], "false"};
             }
-            return "";
-        } catch(Exception ex){
+        } catch (Exception ex) {
             ex.printStackTrace();
         }
-        return "";
+        return new String[]{"", "false"};
     }
+
     
     class LoginUser implements ActionListener{
 
