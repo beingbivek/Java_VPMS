bd6b0003ec1f26e9f23dc80465f3c1c1d0c0ab29
beingbivek <beingbivek@gmail.com>
2025-08-06 15:26:15 +0545

    Added Total column and these are renamed, vehicle number column shows vehicle number and received by shows user name

--- src/vpms/controller/SecurePaymentController.java
+++ src/vpms/controller/SecurePaymentController.java
@@ -1,6 +1,6 @@
 package vpms.controller;
 
-import vpms.dao.PaymentDao;
+import vpms.dao.*;
 import vpms.model.PaymentData;
 import vpms.utils.TableEnhancer;
 import vpms.view.SecurePaymentView;
@@ -20,7 +20,7 @@ public class SecurePaymentController {
     public SecurePaymentController(SecurePaymentView view) {
         this.view = view;
 
-        loadPaymentsTable();                         // first fill
+        loadPaymentsTable();                       
         view.getSearchTextField().addActionListener(e -> filterPayments());
         view.getCancelButton().addActionListener(e -> {
             view.getSearchTextField().setText("");
@@ -30,7 +30,6 @@ public class SecurePaymentController {
 
     public void open() { view.setVisible(true); }
 
-    /* ========== fill + beautify main table ========== */
     private void loadPaymentsTable() {
         List<PaymentData> list = paymentDao.showPayments();
         buildModel(list);
@@ -40,29 +39,32 @@ public class SecurePaymentController {
         String kw = view.getSearchTextField().getText().trim();
         buildModel(paymentDao.searchPayments(kw));
     }
-
-    /* ---------- helper that builds model + styles table ---------- */
+    
     private void buildModel(List<PaymentData> rows) {
 
         DefaultTableModel m = new DefaultTableModel() {
             @Override public boolean isCellEditable(int r,int c){ return false; }
         };
         m.setColumnIdentifiers(new String[]{
-            "Payment ID","Parking ID","Vehicle ID","User ID",
-            "Regular","Demand","Reservation","Extra",
+            "Payment ID","Ticket ID","Vehicle Number","Received By",
+            "Regular","Demand","Reservation","Extra","Total",
             "Status","Time"
         });
+        
+        VehicleDao vDao = new VehicleDao();
+        UserDao uDao = new UserDao();
 
         for (PaymentData p : rows) {
             m.addRow(new Object[]{
                 p.getPayment_id(),
                 p.getParking_id(),
-                p.getVehicle_id(),
-                p.getUser_id(),
+                vDao.getVehicleNumberFromId(p.getVehicle_id()),
+                uDao.getUserNameFromId(p.getUser_id()),
                 p.getRegularPrice(),
                 p.getDemandPrice(),
                 p.getReservationPrice(),
                 p.getExtraCharge(),
+                Double.parseDouble(p.getRegularPrice()) + Double.parseDouble(p.getDemandPrice()) + Double.parseDouble(p.getReservationPrice()) + Double.parseDouble(p.getExtraCharge()),
                 p.getPaymentStatus(),
                 p.getPaymentTime()!=null ? p.getPaymentTime().format(DTF) : ""
             });
@@ -71,7 +73,6 @@ public class SecurePaymentController {
         JTable tbl = view.getPaymentTable();
         tbl.setModel(m);
 
-        /* preferred widths (pixels) */
         TableEnhancer.beautifyTable(tbl,
                 new int[]{70,70,70,60,80,80,90,70,90,160});
     }
